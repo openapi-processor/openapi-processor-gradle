d3eed779d84ef79d30124ec978b4ffbabe563def
Martin Hauner <martin.hauner@gmx.net>
2023-05-29 16:24:33 +0200

    debug build

--- src/testInt/groovy/io/openapiprocessor/gradle/support/Gradle.groovy
+++ src/testInt/groovy/io/openapiprocessor/gradle/support/Gradle.groovy
@@ -8,9 +8,10 @@ package io.openapiprocessor.gradle.support
 class Gradle {
 
     static List<String> VERSIONS = [
-        '7.0'/*, '7.0.1', '7.0.2',
+        '7.0', '7.0.1', '7.0.2',
         '7.1', '7.1.1',
         '7.2',
+            /*
         '7.3', '7.3.1', '7.3.2', '7.3.3',
         '7.4', '7.4.1', '7.4.2',
         '7.5', '7.5.1',
