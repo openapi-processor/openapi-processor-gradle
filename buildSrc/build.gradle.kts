95619fdfb92fd329f680d537d9e59ea417623270
Martin Hauner <martin.hauner@gmx.net>
2025-08-14 14:58:38 +0200

    drop old version plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,13 +6,3 @@ dependencies {
     // catalog hack: https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     // compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
-
-gradlePlugin {
-    plugins {
-        create("VersionPlugin") {
-            id = "openapiprocessor.version"
-            implementationClass = "VersionPlugin"
-        }
-    }
-}
-
