662f022fc807eff322b8daff223ee2bf24098940
Martin Hauner <martin.hauner@gmx.net>
2025-08-14 09:39:23 +0200

    better home for repositoriees

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    mavenCentral()
-    maven {
-      url = uri("https://plugins.gradle.org/m2/")
-    }
-}
-
 dependencies {
     // catalog hack: https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     // compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,23 @@
+rootProject.name = "buildSrc"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven {
+          url = uri("https://plugins.gradle.org/m2/")
         }
     }
 }
